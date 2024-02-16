81ffbe6cef70e9240591414fd545efba04049ec2
Alex Shepard <alex@meatfreezer.com>
2024-02-16 13:42:20 -0800

    update base config
    will move this to a config file soon

--- train_geo_model.py
+++ train_geo_model.py
@@ -16,19 +16,20 @@ from lib.geo_dataset_maker import DiscretizedInatGeoModelDataset
 
 def main():
     params = {
-        "export_dir": "/Users/alex/Desktop/Machine Learning Development/vision-export-20221020154449-Galliformes-573",
-        "export_short_version": "galliformes",
+        "export_dir": "/data-ssd/alex/datasets/vision-export-20231231060015-aka-2.11",
+        "export_short_version": "2.11",
         "train_only_cid_data": True,
         "train_only_wild_data": False,
         "h3_resolution": 6,
         "num_random_samples": 100_000,
-        "elevation_file": "/Users/alex/Desktop/Machine Learning Development/elevation_h3_resolution6.csv",
-        "experiment_dir": "/Users/alex/Desktop/Machine Learning Development/geo_model_macos/2_8_macos",
+        "elevation_file": "/home/alex/elevation_h3_resolution6.csv",
+        "experiment_dir": "/data-ssd/alex/experiments/geo_prior_tf/2_11",
         "batch_size": 1024,
-        "num_epochs": 10,
+        "num_epochs": 200,
         "initial_lr": 0.0005,
         "shuffle_buffer_size": 50_000,
         "full_shuffle_before_tfrecords": False,
+        "lr_warmup_cosine_decay": True,
     }
 
     ds = DiscretizedInatGeoModelDataset(config=params)
