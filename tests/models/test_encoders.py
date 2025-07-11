96fad19e093b113bfb8d8eaa20082f41929479c5
Alex Shepard <alex@meatfreezer.com>
2025-07-11 15:25:18 -0700

    test that input feats have correct shape
    when using environmental data as well

--- tests/models/test_encoders.py
+++ tests/models/test_encoders.py
@@ -1,13 +1,21 @@
+import numpy as np
 import pytest
 
 from lib.models import encoders
 
-@pytest.mark.parametrize("strategy, expected_feats", [
-    ("sinusoidal", 4),
-    ("sinusoidal_hd", 14),
+bioclim_raster = np.random.random((2160, 4320, 20)).astype(np.float32)
+elev_raster = np.random.random((2160, 4320, 1)).astype(np.float32)
+
+@pytest.mark.parametrize("strategy, raster, expected_feats", [
+    ("sinusoidal", None, 4),
+    ("sinusoidal_hd", None, 14),
+    ("sinusoidal", bioclim_raster, 24),
+    ("sinusoidal_hd", bioclim_raster, 34),
+    ("sinusoidal", elev_raster, 5),
+    ("sinusoidal_hd", elev_raster, 15),
 ])
-def test_coord_encoder_input_feats(strategy, expected_feats):
-    encoder = encoders.CoordEncoder(strategy)
+def test_coord_encoder_input_feats(strategy, raster, expected_feats):
+    encoder = encoders.CoordEncoder(strategy, raster)
     assert encoder.num_input_feats() == expected_feats
 
 
