e5e04837300b2de1bafc29022a5131d9b0cbbfab
Alex Shepard <alex@meatfreezer.com>
2025-07-14 12:43:31 -0700

    add a unit test for polar continuity bug
    the bug is that our non-hd encoding has a continuity
    at the north and south poles due to a trig error
    
    confirm the bug with non-hd encoding
    confirm that hd encoding fixes the bug

--- tests/models/test_encoders.py
+++ tests/models/test_encoders.py
@@ -19,4 +19,29 @@ def test_coord_encoder_input_num_feats(strategy, raster, expected_feats):
     assert encoder.num_input_feats() == expected_feats
 
 
-   
+def test_coord_encoder_hd_fix_polar_continuitiy():
+    north_pole_coords = np.array([[0.01, 90.0]])
+    south_pole_coords = np.array([[0.01, -90.0]])
+
+    # start by confirming the polar continuity with non-hd inputs
+    encoder = encoders.CoordEncoder("sinusoidal", None)
+    encoded_north_pole = encoder.encode(north_pole_coords)
+    encoded_south_pole = encoder.encode(south_pole_coords)
+
+    assert np.allclose(
+        encoded_north_pole, 
+        encoded_south_pole
+    )
+
+    # now confirm that there is no polar continuity with hd inputs
+    hd_encoder = encoders.CoordEncoder("sinusoidal_hd", None)
+    hd_encoded_north_pole = hd_encoder.encode(north_pole_coords)
+    hd_encoded_south_pole = hd_encoder.encode(south_pole_coords)
+    assert not np.allclose(
+        hd_encoded_north_pole,
+        hd_encoded_south_pole
+    )
+
+    
+
+
